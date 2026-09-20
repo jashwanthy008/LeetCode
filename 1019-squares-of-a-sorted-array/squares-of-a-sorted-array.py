41cee1c7d76a546cc01d5c34e32c63931f9c2cf8
Jashwanth Y <jashwanthy008@gmail.com>
2026-09-20 19:57:15 +0530

    Time: 31 ms (5.39%) | Memory: 14.5 MB (10.26%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,9 +4,39 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nu=[]
+        num1=[]
+        num2=[]
+        num3=[]
+
         for i in range(len(nums)):
-            soo=nums[i]*nums[i]
-            nu.append(soo)
-        ni=sorted(nu)
-        return ni
+            if nums[i]<0:
+                num1.append(nums[i])
+            else:
+                num2.append(nums[i])
+        for i in range(len(num1)):
+            num1[i]=num1[i]*num1[i]
+        for j in range(len(num2)):
+            num2[j]=num2[j]*num2[j]
+        num1=num1[::-1]
+        left=0
+        right=0
+
+        while left<len(num1) and right<len(num2):
+            i=num1[left]
+            j=num2[right]
+            if i<j:
+                num3.append(i)
+                left+=1
+            else:
+                num3.append(j)
+                right+=1
+
+        while left<len(num1):
+            num3.append(num1[left])
+            left+=1
+
+        while right<len(num2):
+            num3.append(num2[right])
+            right+=1
+        
+        return num3
