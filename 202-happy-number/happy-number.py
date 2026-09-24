62fa942e5c657d7f89b4f720eb75726fcd5e271f
Jashwanth Y <jashwanthy008@gmail.com>
2026-09-24 23:36:36 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (55.82%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        sett=set()
+
+        while n!=1:
+            if n in sett:
+                return False
+            sett.add(n)
+            summ=0
+            for i in str(n):
+                summ+=int(i)*int(i)
+            n=summ
+        return True
